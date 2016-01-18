ffc2a071e0ee0dfe377243672b5d6f60be724ea8
Dave <david_rowe@bathnes.gov.uk>
2016-01-18 22:27:55 +0000

    Some work on arena

--- connectors/arena.js
+++ connectors/arena.js
@@ -1,4 +1,4 @@
-console.log('durham connector loading...');
+console.log('arena connector loading...');
 
 ///////////////////////////////////////////
 // REQUIRES
@@ -33,19 +33,46 @@ exports.searchByISBN = function (isbn, lib, callback) {
         // Mega Hack! Find occurence of search_item_id= and then &agency_name=, and get item ID inbetween
         var itemId = response.substring(response.lastIndexOf("search_item_id=") + 15, response.lastIndexOf("&agency_name="));
         if (itemId) {
-            var url = lib.Url + 'results?p_p_state=normal&p_p_lifecycle=1&p_p_action=1&p_p_id=crDetailWicket_WAR_arenaportlets&p_p_col_count=4&p_p_col_id=column-2&p_p_col_pos=1&p_p_mode=view&facet_queries=&search_item_no=0&sort_advice=field%3DRelevance%26direction%3DDescending&search_item_id=' + itemId + '&agency_name=AUK000226&search_type=solr&search_query=organisationId_index%3AAUK000226%7C6+AND+number_index%3A9780747532743';
-            // Call the item page.
-            request.get({ forever: true, url: url, timeout: 10000, headers: { 'Connection': 'keep-alive' }, jar: true }, function (error, message, response) {
-                // Request 3: After triggering the item page, we should then be able to get the availability container XML data
+            // var url = lib.Url + 'results?p_p_state=normal&p_p_lifecycle=1&p_p_action=1&p_p_id=crDetailWicket_WAR_arenaportlets&p_p_col_count=4&p_p_col_id=column-2&p_p_col_pos=1&p_p_mode=view&facet_queries=&search_item_no=0&sort_advice=field%3DRelevance%26direction%3DDescending&search_item_id=' + itemId + '&agency_name=AUK000226&search_type=solr&search_query=organisationId_index%3AAUK000226%7C6+AND+number_index%3A' + isbn;
+            var url = lib.Url + 'results?p_p_state=normal&p_p_lifecycle=1&p_p_action=1&p_p_id=crDetailWicket_WAR_arenaportlets&p_p_col_count=4&p_p_col_id=column-2&p_p_col_pos=1&p_p_mode=view&search_item_no=0&search_item_id=' + itemId + '&search_type=solr';
+            // Request: Get the item page.
+            request.get({ forever: true, url: url, timeout: 10000, headers: { 'Connection': 'keep-alive' }, jar: true }, function (error, message, response) {              
+                var resourceId = '/crDetailWicket/?wicket:interface=:0:recordPanel:holdingsPanel::IBehaviorListener:0:';
                 var headers = { 'Accept': 'text/xml', 'Wicket-Ajax': true };
-                url = lib.Url + 'results?p_p_id=crDetailWicket_WAR_arenaportlets&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=%2FcrDetailWicket%2F%3Fwicket%3Ainterface%3D%3A0%3ArecordPanel%3AholdingsPanel%3A%3AIBehaviorListener%3A0%3A&p_p_cacheability=cacheLevelPage&p_p_col_id=column-2&p_p_col_pos=1&p_p_col_count=3';
+                url = lib.Url + 'results?p_p_id=crDetailWicket_WAR_arenaportlets&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=' + resourceId + ' &p_p_cacheability=cacheLevelPage&p_p_col_id=column-2&p_p_col_pos=1&p_p_col_count=3';
+                // Request 3: After triggering the item page, we should then be able to get the availability container XML data
                 request.get({ forever: true, url: url, headers: headers, timeout: 10000, jar: true }, function (error, message, response) {
                     if (handleError(error)) return;
                     xml2js.parseString(response, function (err, res) {
-                        
-                        console.log(res['ajax-response'].component);
-                        //$ = cheerio.load(res['ajax-response']);
-                      
+                        $ = cheerio.load(res['ajax-response'].component[0]._);
+                        // There's gotta be a better way of doing this
+                        $('.arena-holding-container a').each(function (index) {
+                            // Details can return multiple organisation for joint services -- want to just get the right one.
+                            if ($(this).text().trim() == lib.OrganisationName) {
+                                headers['Wicket-FocusedElementId'] = 'id__crDetailWicket__WAR__arenaportlets____2a';
+                                resourceId = '/crDetailWicket/?wicket:interface=:0:recordPanel:holdingsPanel:content:holdingsView:1:holdingContainer:togglableLink::IBehaviorListener:0:';
+                                url = lib.Url + 'results?p_p_id=crDetailWicket_WAR_arenaportlets&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=' + resourceId + '&p_p_cacheability=';
+
+                                // Request 4: (Looped) Get the holdings details for that organisation. 
+                                request.get({ forever: true, headers: headers, url: url, timeout: 10000, jar: true }, function (error, message, response) {
+                                    xml2js.parseString(response, function (err, res) {
+                                        $ = cheerio.load(res['ajax-response'].component[0]._);
+
+                                        var libs = {};
+                                        $('.arena-holding-container').each(function (index) {
+
+                                            resourceId = '/crDetailWicket/?wicket:interface=:' + index + ':recordPanel:holdingsPanel:content:holdingsView:1:childContainer:childView:0:holdingPanel:holdingContainer:togglableLink::IBehaviorListener:0:';
+                                            url = lib.Url + 'results?p_p_id=crDetailWicket_WAR_arenaportlets&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=' + resourceId + '&p_p_cacheability=';
+
+                                            // Request 5: (Looped) Get the libraries availability
+                                            request.get({ forever: true, headers: headers, url: url, timeout: 10000, jar: true }, function (error, message, response) {
+                                                console.log(response);
+                                            });
+                                        });
+                                    });
+                                });
+                            }
+                        });
                     });
                 });
             });
