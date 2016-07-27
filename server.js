b362dec6cfc26cfa7e8ff62e2d991cf689ac52b4
Dave <david_rowe@bathnes.gov.uk>
2016-07-27 23:24:44 +0100

    Adding front end

--- server.js
+++ server.js
@@ -5,21 +5,48 @@
 var express = require('express');
 var libraries = require('./libraries');
 
+var app = express();
 ///////////////////////////////////////////
-// Routes
+// Static files
+// Setup for serving static content
+///////////////////////////////////////////
+app.use(express.static('public'));
+
+///////////////////////////////////////////
+// Template engine
+// Using pug as a basic templating engine
+///////////////////////////////////////////
+app.set('views', './views')
+app.set('view engine', 'pug');
+
+///////////////////////////////////////////
+// Routes - Pages
+// Routes to deliver pages.
+///////////////////////////////////////////
+app.get('/', function (req, res) {
+    res.render('index', { title: 'Hey', message: 'Hello there!' });
+});
+
+///////////////////////////////////////////
+// Routes - Web service
 // The web service routes
 ///////////////////////////////////////////
-var app = express();
 app.get('/services', libraries.getServices);
 app.get('/libraries', libraries.getLibraries);
 app.get('/availabilityByISBN/:isbn', libraries.isbnSearch);
 
-// For maintenance
+///////////////////////////////////////////
+// Routes - Maintenance
+// Routes for maintaining the data
+///////////////////////////////////////////
 // app.get('/refreshbranches', libraries.refreshBranches)
 // app.get('/refreshservicetwitters', libraries.refreshServiceTwitters);
 // app.get('/refreshbranchtwitters', libraries.refreshBranchTwitters);
 
-// For testing
+///////////////////////////////////////////
+// Routes - Testing
+// Tests
+///////////////////////////////////////////
 app.get('/testAvailabilityByISBN', libraries.testIsbnSearch);
 
 ///////////////////////////////////////////
