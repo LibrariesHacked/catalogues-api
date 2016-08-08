d76e5cbe3f52689971c904486847ebdb5e00646f
Dave <dave_roweuk@yahoo.co.uk>
2016-08-08 23:59:14 +0100

    working on front end

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+﻿var gulp = require('gulp');
+
+gulp.task('default', function () {
+    gulp.src([
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',
+        'node_modules/bootstrap-3-typeahead/bootstrap3-typeahead.min.js',
+        'node_modules/jquery/dist/jquery.min.js'
+    ]).pipe(gulp.dest('public/libs/js'));
+    gulp.src([
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',
+        'node_modules/bootswatch/paper/bootstrap.min.css'
+    ]).pipe(gulp.dest('public/libs/css'));
+});
