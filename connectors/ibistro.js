47d2e91745655f95130ea0802fd7e22908f5745c
Dave <DaveBathnes@users.noreply.github.com>
2021-01-09 23:25:29 +0000

    #8 Getting ibistro working for Hull

--- connectors/ibistro.js
+++ connectors/ibistro.js
@@ -1,91 +1,63 @@
-/// ////////////////////////////////////////
-// IBISTRO
-//
-/// ////////////////////////////////////////
+
+const axios = require('axios').default
+const cheerio = require('cheerio')
+const common = require('../connectors/common')
+
 console.log('ibistro connector loading...')
 
-/// ////////////////////////////////////////
-// REQUIRES
-// Request (for HTTP calls) and cheerio for
-// querying the HTML returned.
-/// ////////////////////////////////////////
-var request = require('request')
-var cheerio = require('cheerio')
-var common = require('../connectors/common')
+/**
+ * Gets the object representing the service
+ * @param {object} service
+ */
+exports.getService = (service) => { return common.getService(service) }
 
-/// ////////////////////////////////////////
-// VARIABLES
-/// ////////////////////////////////////////
+/**
+ * Gets the libraries in the service based upon possible search and filters within the library catalogue
+ * @param {object} service
+ */
+exports.getLibraries = async function (service) {
+  const responseLibraries = common.initialiseGetLibrariesResponse(service)
 
-/// ////////////////////////////////////////
-// Function: getService
-/// ////////////////////////////////////////
-exports.getService = function (svc) {
-  var service = common.getService(svc)
-  service.CatalogueUrl = svc.Url + svc.Home
-  return service
-}
-
-/// ////////////////////////////////////////
-// Function: getLibraries
-/// ////////////////////////////////////////
-exports.getLibraries = function (service, callback) {
-  var responseLibraries = { service: service.Name, code: service.Code, libraries: [], start: new Date() }
-  // Request 1: Get advanced search page
-  request.get({ url: service.Url + service.Home, timeout: 60000 }, function (error, message, response) {
-    if (common.handleErrors(callback, responseLibraries, error, message)) return
-    $ = cheerio.load(response)
-    $('#library option').each(function () {
-      if ($(this).text().indexOf('ALL') == -1 && $(this).text().indexOf('HERE') == -1) responseLibraries.libraries.push($(this).text())
-    })
-    common.completeCallback(callback, responseLibraries)
+  const homePage = await axios.get(service.Url + service.Home, { timeout: 60000 })
+  const $ = cheerio.load(homePage.data)
+  $('#library option').each((idx, option) => {
+    if (common.isLibrary($(option).text())) responseLibraries.libraries.push($(option).text())
   })
+  return common.endResponse(responseLibraries)
 }
 
-/// ///////////////////////
-// Function: searchByISBN
-/// ///////////////////////
-exports.searchByISBN = function (isbn, lib, callback) {
-  var responseHoldings = { service: lib.Name, code: lib.Code, availability: [], start: new Date(), url: lib.Url + lib.Search + isbn }
+/**
+ * Retrieves the availability summary of an ISBN by library
+ * @param {string} isbn
+ * @param {object} service
+ */
+exports.searchByISBN = async function (isbn, service) {
+  const responseHoldings = common.initialiseSearchByISBNResponse(service)
+  responseHoldings.url = service.Url + service.Search + isbn
 
-  // Declare this for use later on depending on search results.
-  var getAvailability = function (itemPage) {
-    $ = cheerio.load(itemPage)
-    var libs = {}
-    var currentLib = ''
-    $('tr').each(function (index, elem) {
-      var libr = $(this).find('td.holdingsheader,th.holdingsheader').eq(0).text().trim()
-      if (libr == 'Copies') libr = $(this).find('.holdingsheader_users_library').eq(0).text().trim()
+  const searchPageRequest = await axios.get(responseHoldings.url, { timeout: 30000 })
+  const itemPage = searchPageRequest.data
 
-      var status = $(this).find('td').eq(3).text().trim()
-      if (libr) {
-        currentLib = libr
-      } else {
-        if (currentLib && status) {
-          if (!libs[currentLib]) libs[currentLib] = { available: 0, unavailable: 0 }
-          lib.Available.indexOf(status) > -1 ? libs[currentLib].available++ : libs[currentLib].unavailable++
-        }
-      }
-    })
-    for (var l in libs) responseHoldings.availability.push({ library: l, available: libs[l].available, unavailable: libs[l].unavailable })
-    common.completeCallback(callback, responseHoldings)
+  let $ = cheerio.load(itemPage)
+
+  if ($('form[name=hitlist]').length > 0) {
+    const itemUrl = service.Url + $('form[name=hitlist]').attr('action')
+    const itemPageRequest = axios.post(itemUrl, 'first_hit=1&form_type=&last_hit=2&VIEW%5E1=Details', { timeout: 30000 })
+    $ = cheerio.load(itemPageRequest.data)
   }
 
-  // Request 1:
-  request.get({ url: responseHoldings.url, timeout: 30000 }, function (error, msg, response) {
-    if (common.handleErrors(callback, responseHoldings, error, msg)) return
-    $ = cheerio.load(response)
-    // Could be multiple copies held - check for a hitlist form
-    if ($('form[name=hitlist]').length > 0) {
-      // By default we'll get the first item - will probably extend this to loop through and get them all.
-      var url = msg.request.uri.protocol + '//' + msg.request.uri.host + $('form[name=hitlist]').attr('action')
-      // Request 2:
-      request.post({ url: url, body: 'first_hit=1&form_type=&last_hit=2&VIEW%5E1=Details', timeout: 30000 }, function (error, message, response) {
-        if (common.handleErrors(callback, responseHoldings, error, message)) return
-        getAvailability(response)
-      })
-    } else {
-      getAvailability(response)
+  var libs = {}
+  var currentLib = ''
+  $('tr').each((idx, tr) => {
+    var libr = $(tr).find('td.holdingsheader,th.holdingsheader').eq(0).text().trim()
+    if (libr === 'Copies') libr = $(tr).find('.holdingsheader_users_library').eq(0).text().trim()
+    var status = $(tr).find('td').eq(3).text().trim()
+    if (libr) currentLib = libr
+    if (!libr && status) {
+      if (!libs[currentLib]) libs[currentLib] = { available: 0, unavailable: 0 }
+      service.Available.indexOf(status) > -1 ? libs[currentLib].available++ : libs[currentLib].unavailable++
     }
   })
+  for (var l in libs) responseHoldings.availability.push({ library: l, available: libs[l].available, unavailable: libs[l].unavailable })
+  return common.endResponse(responseHoldings)
 }
