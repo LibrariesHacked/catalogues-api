d81f1d3221b29e629ec5bab6e999e5859c67bd57
Dave <DaveBathnes@users.noreply.github.com>
2021-01-11 23:00:00 +0000

    #8 Refactoring prism3

--- connectors/prism3.js
+++ connectors/prism3.js
@@ -1,70 +1,57 @@
 
+const axios = require('axios')
+const cheerio = require('cheerio')
+const common = require('../connectors/common')
+
 console.log('prism3 connector loading...')
-/// ////////////////////////////////////////
-// REQUIRES
-// Request (for HTTP calls) and cheerio for
-// querying the HTML returned.
-/// ////////////////////////////////////////
-var request = require('request')
-var cheerio = require('cheerio')
-var common = require('../connectors/common')
 
-/// ////////////////////////////////////////
-// VARIABLES
-/// ////////////////////////////////////////
-var reqHeader = { 'Content-Type': 'text/xml; charset=utf-8' }
+const HEADER = { 'Content-Type': 'text/xml; charset=utf-8' }
 
-/// ////////////////////////////////////////
-// Function: getService
-/// ////////////////////////////////////////
-exports.getService = function (svc) {
-  var service = common.getService(svc)
-  return service
+/**
+ * Gets the object representing the service
+ * @param {object} service
+ */
+exports.getService = (service) => { return common.getService(service) }
+
+/**
+ * Gets the libraries in the service based upon possible search and filters within the library catalogue
+ * @param {object} service
+ */
+exports.getLibraries = async function (service) {
+  const responseLibraries = common.initialiseGetLibrariesResponse(service)
+
+  const advancedSearchPageRequest = await axios.get(service.Url + 'advancedsearch?target=catalogue', { timeout: 60000 })
+
+  const $ = cheerio.load(advancedSearchPageRequest.data)
+  $('#locdd option').each((idx, option) => {
+    if ($(option).text() !== '') responseLibraries.libraries.push($(option).text())
+  })
+
+  return common.endResponse(responseLibraries)
 }
 
-/// ////////////////////////////////////////
-// Function: getLibraries
-/// ////////////////////////////////////////
-exports.getLibraries = function (service, callback) {
-  var responseLibraries = { service: service.Name, code: service.Code, libraries: [], start: new Date() }
+/**
+ * Retrieves the availability summary of an ISBN by library
+ * @param {string} isbn
+ * @param {object} service
+ */
+exports.searchByISBN = async function (isbn, service) {
+  const responseHoldings = common.initialiseSearchByISBNResponse(service)
 
-  // Request 1: Get advanced search page
-  request.get({ url: service.Url + 'advancedsearch?target=catalogue', timeout: 60000 }, function (error, message, response) {
-    if (common.handleErrors(callback, responseLibraries, error, message)) return
-    $ = cheerio.load(response)
-    $('#locdd option').each(function () {
-      if ($(this).text() != '') responseLibraries.libraries.push($(this).text())
-    })
-    common.completeCallback(callback, responseLibraries)
-  })
-}
+  const searchRequest = await axios.get(service.Url + 'items.json?query=' + isbn, { headers: HEADER, timeout: 30000 })
 
-/// ///////////////////////
-// Function: searchByISBN
-/// ///////////////////////
-exports.searchByISBN = function (isbn, lib, callback) {
-  var responseHoldings = { service: lib.Name, code: lib.Code, availability: [], start: new Date() }
-  // Request 1:
-  request.get({ url: lib.Url + 'items.json?query=' + isbn, headers: reqHeader, timeout: 30000 }, function (error, msg, response) {
-    if (common.handleErrors(callback, responseHoldings, error, msg)) return
-    var res = JSON.parse(response)
-    var itemUrl = Object.keys(res)[2]
-    if (!itemUrl) {
-      common.completeCallback(callback, responseHoldings)
-      return
-    }
-    // Request 2:
-    request.get({ url: itemUrl, headers: reqHeader, timeout: 30000 }, function (error, msg, res) {
-      if (common.handleErrors(callback, responseHoldings, error, msg)) return
-      $ = cheerio.load(res)
-      $('#availability').find('ul.options li').each(function (i, elem) {
-        var libr = { library: $(this).find('h3 span span').text().trim(), available: 0, unavailable: 0 }
-        $(this).find('div.jsHidden table tbody tr').each(function (i, elem) {
-          lib.Available.indexOf($(this).find('td.item-status span').text().trim()) > -1 ? libr.available++ : libr.unavailable++
-        })
-        responseHoldings.availability.push(libr)
-      })
-      common.completeCallback(callback, responseHoldings)
+  if (searchRequest.data.length === 0) return common.endResponse(responseHoldings)
+  var itemUrl = Object.keys(searchRequest.data)[2]
+
+  const itemRequest = await axios.get(itemUrl, { headers: HEADER, timeout: 30000 })
+  const $ = cheerio.load(itemRequest.data)
+  $('#availability').find('ul.options li').each((idx, li) => {
+    var libr = { library: $(li).find('h3 span span').text().trim(), available: 0, unavailable: 0 }
+    $(li).find('div.jsHidden table tbody tr').each((i, tr) => {
+      service.Available.indexOf($(tr).find('td.item-status span').text().trim()) > -1 ? libr.available++ : libr.unavailable++
     })
+    responseHoldings.availability.push(libr)
   })
+
+  return common.endResponse(responseHoldings)
 }
