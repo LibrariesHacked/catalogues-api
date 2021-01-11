fcb08aab4017012e9e2d577d52283c6fbf31cc7d
Dave <DaveBathnes@users.noreply.github.com>
2021-01-11 10:26:35 +0000

    #8 Getting koha working for Cheshire and East

--- connectors/koha.js
+++ connectors/koha.js
@@ -1,74 +1,64 @@
-/// ////////////////////////////////////////
-// KOHA
-//
-/// ////////////////////////////////////////
+const axios = require('axios').default
+const cheerio = require('cheerio')
+const common = require('../connectors/common')
+
 console.log('koha connector loading...')
 
-/// ////////////////////////////////////////
-// REQUIRES
-// Request (for HTTP calls) and cheerio for
-// querying the HTML returned.
-/// ////////////////////////////////////////
-var request = require('request')
-var cheerio = require('cheerio')
-var common = require('../connectors/common')
+const CAT_URL = 'cgi-bin/koha/opac-search.pl?format=rss2&idx=nb&q='
+const LIBS_URL = 'cgi-bin/koha/opac-search.pl?[MULTIBRANCH]do=Search&expand=holdingbranch#holdingbranch_id'
 
-/// ///////////////////////
-// VARIABLES
-/// ///////////////////////
-var catUrl = 'cgi-bin/koha/opac-search.pl?format=rss2&idx=nb&q='
-var libsUrl = 'cgi-bin/koha/opac-search.pl?[MULTIBRANCH]do=Search&expand=holdingbranch#holdingbranch_id'
+/**
+ * Gets the object representing the service
+ * @param {object} service
+ */
+exports.getService = (service) => { return common.getService(service) }
 
-/// ////////////////////////////////////////
-// Function: getService
-/// ////////////////////////////////////////
-exports.getService = function (svc) {
-  var service = common.getService(svc)
-  return service
+/**
+ * Gets the libraries in the service based upon possible search and filters within the library catalogue
+ * @param {object} service
+ */
+exports.getLibraries = async function (service) {
+  const responseLibraries = common.initialiseGetLibrariesResponse(service)
+
+  const url = service.Url + (service.MultiBranchLimit ? LIBS_URL.replace('[MULTIBRANCH]', 'multibranchlimit=' + service.MultiBranchLimit + '&') : LIBS_URL.replace('[MULTIBRANCH]', ''))
+  const libraryPageRequest = await axios.get(url, { timeout: 60000 })
+  const $ = cheerio.load(libraryPageRequest.data)
+  $('div#location select#branchloop option').each((idx, option) => {
+    if (common.isLibrary($(option).text())) responseLibraries.libraries.push($(this).text().trim())
+  })
+  $('li#holdingbranch_id ul li span.facet-label').each((idx, label) => {
+    responseLibraries.libraries.push($(label).text().trim())
+  })
+  $('li#homebranch_id ul li span.facet-label').each((idx, label) => {
+    responseLibraries.libraries.push($(label).text().trim())
+  })
+
+  return common.endResponse(responseLibraries)
 }
 
-/// ////////////////////////////////////////
-// Function: getLibraries
-/// ////////////////////////////////////////
-exports.getLibraries = function (service, callback) {
-  var responseLibraries = { service: service.Name, code: service.Code, libraries: [], start: new Date() }
-  // Request 1: Call search page with option set to expand branches.
-  request.get({ url: service.Url + (service.MultiBranchLimit ? libsUrl.replace('[MULTIBRANCH]', 'multibranchlimit=' + service.MultiBranchLimit + '&') : libsUrl.replace('[MULTIBRANCH]', '')), timeout: 60000 }, function (error, message, response) {
-    if (common.handleErrors(callback, responseLibraries, error, message)) return
-    $ = cheerio.load(response)
-    $('div#location select#branchloop option').each(function () {
-      if ($(this).text() != 'All libraries') responseLibraries.libraries.push($(this).text().trim())
-    })
-    $('li#holdingbranch_id ul li span.facet-label').each(function () {
-      responseLibraries.libraries.push($(this).text().trim())
-    })
-    common.completeCallback(callback, responseLibraries)
-  })
-}
+/**
+ * Retrieves the availability summary of an ISBN by library
+ * @param {string} isbn
+ * @param {object} service
+ */
+exports.searchByISBN = async function (isbn, service) {
+  const responseHoldings = common.initialiseSearchByISBNResponse(service)
+  responseHoldings.url = service.Url + CAT_URL + isbn
 
-/// ///////////////////////
-// Function: searchByISBN
-/// ///////////////////////
-exports.searchByISBN = function (isbn, lib, callback) {
-  var responseHoldings = { service: lib.Name, code: lib.Code, availability: [], start: new Date(), url: lib.Url + catUrl + isbn }
-  // Request 1: The ISBN search
-  request.get({ url: responseHoldings.url, timeout: 30000 }, function (error, msg, res) {
-    if (common.handleErrors(callback, responseHoldings, error, msg)) return
-    $ = cheerio.load(res, { normalizeWhitespace: true, xmlMode: true })
-    var bibLink = $('guid').text()
-    if (!bibLink) { common.completeCallback(callback, responseHoldings); return }
-    // Request 2: The item link
-    request.get({ url: bibLink + '&viewallitems=1', timeout: 30000, rejectUnauthorized: (!lib.IgnoreSSL) }, function (error, msg, res) {
-      if (common.handleErrors(callback, responseHoldings, error, msg)) return
-      $ = cheerio.load(res)
-      var libs = {}
-      $('.holdingst tbody').find('tr').each(function (i, elem) {
-        var lib = $(this).find('td.location span span').eq(1).text().trim()
-        if (!libs[lib]) libs[lib] = { available: 0, unavailable: 0 }
-        $(this).find('td.status span').text().trim() == 'Available' ? libs[lib].available++ : libs[lib].unavailable++
-      })
-      for (var l in libs) responseHoldings.availability.push({ library: l, available: libs[l].available, unavailable: libs[l].unavailable })
-      common.completeCallback(callback, responseHoldings)
-    })
+  const searchPageRequest = await axios.get(responseHoldings.url, { timeout: 30000 })
+  let $ = cheerio.load(searchPageRequest.data, { normalizeWhitespace: true, xmlMode: true })
+  var bibLink = $('guid').text()
+  if (!bibLink) return common.endResponse(responseHoldings)
+
+  const itemPageRequest = await axios.get(bibLink + '&viewallitems=1', { timeout: 30000 })
+  $ = cheerio.load(itemPageRequest.data)
+  const libs = {}
+  $('.holdingst tbody').find('tr').each((idx, table) => {
+    var lib = $(table).find('td.location span span').eq(1).text().trim()
+    if (!libs[lib]) libs[lib] = { available: 0, unavailable: 0 }
+    $(table).find('td.status span').text().trim() === 'Available' ? libs[lib].available++ : libs[lib].unavailable++
   })
+  for (var l in libs) responseHoldings.availability.push({ library: l, available: libs[l].available, unavailable: libs[l].unavailable })
+
+  return common.endResponse(responseHoldings)
 }
