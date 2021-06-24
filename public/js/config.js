4901eccbb9c35187aacd1ab141d3bea718e75059
Dave <8709533+DaveBathnes@users.noreply.github.com>
2021-06-24 23:03:12 +0100

    #17 Initial work on dependencies for front end

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,3 @@
 var config = {
-  booksKey: '',
-  mapBoxToken: '',
-  mapTilesLight: 'https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}@2x?access_token=',
-  mapAttribution: '&copy; Mapbox, &copy; OpenStreetMap',
-  bngProjection: '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs'
+  booksKey: ''
 }
